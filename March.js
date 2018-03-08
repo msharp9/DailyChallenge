00111c5d6349e6dc1e8e3cbb9946c811939faf64
msharp9 <msharp9@outlook.com>
2018-03-07 20:13:02 -0700

    React Kata? awesome

--- March.js
+++ March.js
@@ -1,3 +1,22 @@
 // Didn't want the file to just get too big.
 // square a number w/o Math, *, or + operators
 sq=x=>x/(1/x)- -.5|0
+
+
+//Apparently there are react Kata's so that's cool:
+const React = require('react');
+
+class States extends React.Component {
+  constructor() {
+    super();
+    this.state = {united: false}
+  }
+  unite() {
+    this.setState({united: true});
+  }
+  render() {
+    const united = this.state.united;
+    let text = united ? 'Code for everyone' : 'Make America code again';
+    return <div className="status">{text}</div>
+  }
+}
