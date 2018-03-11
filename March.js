3ae599dc0fbfdb5fd504f38e3fa47ad1bac52de1
msharp9 <msharp9@outlook.com>
2018-03-10 17:11:49 -0700

    Hello world react

--- March.js
+++ March.js
@@ -34,3 +34,18 @@ const EggList = (props) => {
 const EasterEgg = (props) => {
   return <li>{props.name}</li>
 };
+
+
+// hello world in React
+const React = require('react');
+const Hello=()=> {
+  return <h1>Hello</h1>
+}
+const World=()=> {
+  return <h2>World!</h2>
+}
+class Greet extends React.Component {
+  render() {
+    return <div><Hello /><World /></div>
+  }
+}
