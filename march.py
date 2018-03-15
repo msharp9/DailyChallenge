12be6c746f6b60a6cebc5cdc9091579385e70f6b
msharp9 <msharp9@outlook.com>
2018-03-14 19:30:45 -0600

    added string translate funciton

--- march.py
+++ march.py
@@ -3,3 +3,11 @@ from random import random
 
 def random_case(x):
     return ''.join([l.upper() if random() < 0.5 else l.lower() for l in x])
+
+
+# string cleaning
+def correct(string):
+    return string.replace('0', 'O').replace('1', 'I').replace('5', 'S')
+# or
+def correct(string):
+    return string.translate(str.maketrans("501", "SOI"))
