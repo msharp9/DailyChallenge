683daca81f614280fc40b563834455efa3955154
msharp9 <msharp9@outlook.com>
2018-03-15 20:05:33 -0600

    Added rotate clockwise function

--- march.py
+++ march.py
@@ -11,3 +11,19 @@ def correct(string):
 # or
 def correct(string):
     return string.translate(str.maketrans("501", "SOI"))
+
+
+
+# Rotate clockwise:
+def rotate_clockwise(matrix):
+    output = [];
+    for i,m in enumerate(matrix):
+        for j,n in enumerate(m):
+            if i > 0:
+                output[j] = n + output[j]
+            else:
+                output.append(n)
+    return output
+# or (yay python!)
+def rotate_clockwise(m):
+    return [''.join(_) for _ in zip(*m[::-1])]
